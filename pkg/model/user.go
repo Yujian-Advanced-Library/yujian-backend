26367fbce40088b63a1496ee560835fda22a43ff
HaoYeRen233 <2708044832@qq.com>
2025-02-25 08:16:40 +0800

    Update user.go
    user的几个返回结构体

--- pkg/model/user.go
+++ pkg/model/user.go
@@ -35,9 +35,31 @@ func (userDO *UserDO) Transfer() *UserDTO {
 }
 
 
-// 修改密码的请求结构体
+// GetUserByIdResponse 根据ID获取用户的返回体
+type GetUserByIdResponse struct {
+	BaseResp
+	Userinfo UserDTO `json:"userinfo"`
+}
+
+// UpdateUserResponse 更新用户信息返回体
+type UpdateUserResponse struct {
+	BaseResp
+}
+
+// UserDeleteResponse 删除用户返回体
+type UserDeleteResponse struct {
+	BaseResp
+	DeleteId int64 `json:"deleted_user_id"` //被删除用户的id,我感觉是不是还是应该返回下这个比较好
+}
+
+// ChangePasswordRequest 修改密码的请求结构体
 type ChangePasswordRequest struct {
 	OldPassword     string `json:"OldPassword"`     // 旧密码
 	NewPassword     string `json:"NewPassword"`     // 新密码
 	ConfirmPassword string `json:"ConfirmPassword"` // 确认新密码
+}
+
+// ChangePasswordResponse 修改密码的返回结构体
+type ChangePasswordResponse struct {
+	BaseResp
 }
