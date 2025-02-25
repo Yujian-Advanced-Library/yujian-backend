20719565a66d467ff4dbe3e21221623f4062183b
HaoYeRen233 <2708044832@qq.com>
2025-02-25 08:46:23 +0800

    Update book_biz.go

--- pkg/biz/book/book_biz.go
+++ pkg/biz/book/book_biz.go
@@ -9,11 +9,18 @@ import (
 )
 
 
+// SearchBooks 搜索书
 func SearchBooks(c *gin.Context) {
 	var req model.BookSearchRequest
+	//绑定
 	if err := c.ShouldBindQuery(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "invalid request parameters",
+		c.JSON(http.StatusBadRequest, model.SearchResponse{
+			BaseResp: model.BaseResp{
+				Error:  err,
+				Code:   http.StatusBadRequest,
+				ErrMsg: "invalid request parameters",
+			},
+			Books: nil,
 		})
 		return
 	}
@@ -27,12 +34,23 @@ func SearchBooks(c *gin.Context) {
 	bookRepository := db.GetBookRepository()
 	books, err := bookRepository.SearchBooks(req.Keyword, req.Category, req.Page, req.PageSize)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "failed to search books",
+		//没查到
+		c.JSON(http.StatusBadRequest, model.SearchResponse{
+			BaseResp: model.BaseResp{
+				Error:  err,
+				Code:   http.StatusNotFound,
+				ErrMsg: "failed to search books",
+			},
+			Books: nil,
 		})
 		return
 	}
 	c.JSON(http.StatusOK, model.SearchResponse{
+		BaseResp: model.BaseResp{
+			Error:  nil,
+			Code:   http.StatusOK,
+			ErrMsg: "",
+		},
 		Books: books,
 	})
 }
@@ -41,9 +59,15 @@ func SearchBooks(c *gin.Context) {
 func GetBookDetail(c *gin.Context) {
 	//获取id
 	bookId, err := strconv.ParseInt(c.Param("bookId"), 10, 64)
+	//解析失败
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "invalid book ID",
+		c.JSON(http.StatusBadRequest, model.BookDetailResponse{
+			BaseResp: model.BaseResp{
+				Error:  err,
+				Code:   http.StatusBadRequest,
+				ErrMsg: "invalid book ID",
+			},
+			Data: model.BookInfoDTO{}, //空
 		})
 		return
 	}
@@ -52,13 +76,23 @@ func GetBookDetail(c *gin.Context) {
 	// 查询详情
 	bookDTO, err := bookRepository.GetBookById(bookId)
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{
-			"error": "book not found",
+		c.JSON(http.StatusNotFound, model.BookDetailResponse{
+			BaseResp: model.BaseResp{
+				Error:  err,
+				Code:   http.StatusNotFound,
+				ErrMsg: "failed to find book",
+			},
+			Data: model.BookInfoDTO{},
 		})
 		return
 	}
-	// 返回
-	c.JSON(http.StatusOK, gin.H{
-		"data": bookDTO,
+	// 找到
+	c.JSON(http.StatusOK, model.BookDetailResponse{
+		BaseResp: model.BaseResp{
+			Error:  nil,
+			Code:   http.StatusOK,
+			ErrMsg: "",
+		},
+		Data: *bookDTO,
 	})
 }
