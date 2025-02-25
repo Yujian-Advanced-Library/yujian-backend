0c3c294b8365c4774a688a1df176d21768eafd46
HaoYeRen233 <2708044832@qq.com>
2025-02-25 09:23:55 +0800

    Update router.go
    书评相关路由

--- pkg/biz/router.go
+++ pkg/biz/router.go
@@ -28,4 +28,12 @@ func SetupRouter(r *gin.Engine) {
 		bookGroup.GET("/:bookId", book.GetBookDetail) // 图书详情获取
 	}
 
+	//书评相关路由
+	reviewsGroup := r.Group("/api/reviews")
+	{
+		reviewsGroup.POST("/post", book.PostReview)              //书评发布接口
+		reviewsGroup.GET("/:bookId", book.GetReviews)            //书评获取接口
+		reviewsGroup.POST("/:reviewId/like", book.LikeonReviews) //书评点赞/踩接口
+	}
+
 }
